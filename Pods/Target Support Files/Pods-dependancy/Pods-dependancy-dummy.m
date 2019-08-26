0f86f23cf7e9b71834a7fc1ded4a0e12133dca3a
Muzammil <muzammilinnovativesol@gmail.com>
2019-08-27 01:13:06 +0500

    Initial Comment

---
+++ Pods/Target Support Files/Pods-dependancy/Pods-dependancy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dependancy : NSObject
+@end
+@implementation PodsDummy_Pods_dependancy
+@end
---
+++ dependancy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dependancy
+//
+//  Created by Muzzamil on 27/08/2019.
+//  Copyright © 2019 Muzammil. All rights reserved.
+//
+
+import UIKit
+
+class TestingController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
